e5d817e860c38b246701ebd8135fd595a115c157
Mitya Zamay <mityazamay@gmail.com>
2022-06-20 18:24:18 +0300

    Шаблон реакт додатку. Додано: відображення даних з сервера, активні елементи вибору

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/app.scss";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+import { useEffect, useState } from "react";
 
 function App() {
+  const url = "https://5970c13810cdc70011cfc08e.mockapi.io/items";
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((item) => (
+              <PizzaBlock pizza={item} key={item.id} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
